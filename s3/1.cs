12509a49f0d6dd12476a0d58c9da3650b2219deb
AlexsandrFK <sasha.furik.241@gmail.com>
2022-03-24 11:15:28 +1200

    3

--- s3/1.cs
+++ s3/1.cs
@@ -1,25 +1,48 @@
-using System;
-
-class Program
+double Distance(double x1, double y1, double z1, double x2, double y2, double z2)
 {
-    static       void Main() {
-    string a = "12821";
-    int b = 0;
-    int c = a.Length;
-    for(int i = a.Length /2;i>0;i--)
+    double xDistance = x1 - x2;
+    double yDistance = y1 - y2;
+    double zDistance = z1 - z2;
+    return Math.Sqrt(Math.Pow(xDistance, 2) + Math.Pow(yDistance, 2) + Math.Pow(zDistance, 2));
+}
+double xA, yA, zA, xB, yB, zB;
+Console.WriteLine("Введите координату x для точки A: ");
+xA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату y для точки A: ");
+yA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату z для точки A: ");
+zA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату x для точки B: ");
+xB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату y для точки B: ");
+yB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату z для точки B: ");
+zB = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Растояние между точками А и В равно: " + Distance(xA, yA, zA, xB, yB, zB));
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+check(number);
+void check(int five)
+{
+    int a = five / 10000;
+    int a1 = five / 1000 % 10;
+    int a3 = five % 100 / 10;
+    int a4 = five % 10;
+    if(a == a4)
     {
-        if(a[b] !=a[c-1]) 
+        if(a1 == a3)
         {
-            Console.WriteLine("Это не полеандром");
-            break;
+            Console.WriteLine("ДА!");
         }
-        else if(a[b] ==a[c-1] && i == 1)
+        else
         {
-            Console.WriteLine("Это полеандром");
-            break;
+            Console.WriteLine("НЕТ!");
         }
-        b++;
-        c--;
     }
+    else
+    {
+        Console.WriteLine("НЕТ!");
     }
-    }
+}
--- s3/2.cs
+++ s3/2.cs
@@ -1,19 +1,23 @@
-﻿using System;
-
-class Program
+﻿double Distance(double x1, double y1, double z1, double x2, double y2, double z2)
 {
-    static   void Main() {
-    int [] A = new []{7,-5,0};
-    int [] B = new []{1,-1,9};
-    int C = 0;
-    for(int i = 0;i<B.Length;i++)
-    {
-      C +=(B[i] - A[i]) * (B[i] - A[i]);
-    }
-
-        Console.WriteLine( Math.Sqrt(C));
-    }
-
-
-
+    double xDistance = x1 - x2;
+    double yDistance = y1 - y2;
+    double zDistance = z1 - z2;
+    return Math.Sqrt(Math.Pow(xDistance, 2) + Math.Pow(yDistance, 2) + Math.Pow(zDistance, 2));
 }
+double xA, yA, zA, xB, yB, zB;
+Console.WriteLine("Введите координату x для точки A: ");
+xA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату y для точки A: ");
+yA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату z для точки A: ");
+zA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату x для точки B: ");
+xB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату y для точки B: ");
+yB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату z для точки B: ");
+zB = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Растояние между точками А и В равно: " + Distance(xA, yA, zA, xB, yB, zB));
